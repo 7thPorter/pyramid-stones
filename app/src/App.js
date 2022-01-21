be1c338db8365c25cc51b023270966e6ca09d9c8
Rusty Porter <rusty.porter@gmail.com>
2022-01-21 14:22:55 -0700

    Updated with MOSTLY working code. Currently, the timer for boats decrements twice as quickly as it should. Working on it.

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useMemo } from "react";
 
 import logo from "./logo.svg";
 import "./App.css";
@@ -13,22 +13,117 @@ function App() {
   const [timeRemaining, setTimeRemaining] = useState(totalSimulationTime);
   const [simulationStarted, setSimulationStarted] = useState(false);
   const [simulationPaused, setSimulationPaused] = useState(false);
+  const [timeToLoad, setTimeToLoad] = useState(6);
+  const [timeToUnload, setTimeToUnload] = useState(12);
+  const [timeOnWater, setTimeOnWater] = useState(10);
+  const [blocksUnloaded, setBlocksUnloaded] = useState(0);
+  const [numberOfBoats, setNumberOfBoats] = useState(20);
+  const [boats, setBoats] = useState([]);
 
-  //I chose to use 'useRef' here rather than useState, becasue
-  const intervalRef = useRef();
+  useEffect(() => {
+    if (boats.length === numberOfBoats) {
+      return;
+    }
+    let newBoats = boats.slice();
+    while (newBoats.length < numberOfBoats) {
+      newBoats.push({ location: "capitol", timer: 0, loaded: false });
+    }
+    while (newBoats.length > numberOfBoats) {
+      newBoats.pop();
+    }
+    setBoats(newBoats);
+  }, [numberOfBoats, boats, setBoats]);
+
+  const lastTimeRemaining = useRef(totalSimulationTime);
+
+  useEffect(() => {
+    if (lastTimeRemaining.current === timeRemaining) {
+      return;
+    }
+
+    lastTimeRemaining.current = timeRemaining;
+
+    setBoats((currentBoats) => {
+      let boats = currentBoats.slice();
+
+      boats.forEach((boat) => {
+        console.log(boat.timer);
+        if (boat.location === "river") {
+          boat.timer = boat.timer - 1;
+          if (boat.timer <= 0) {
+            if (boat.loaded) {
+              boat.location = "capitol";
+              boat.timer = timeToUnload;
+            } else {
+              boat.location = "quarry";
+              boat.timer = timeToLoad;
+            }
+          }
+        } else if (boat.location === "quarry" && boat.loaded === false) {
+          boat.timer = boat.timer - 1;
+          if (boat.timer <= 0) {
+            boat.loaded = true;
+          }
+        } else if (boat.location === "capitol" && boat.loaded) {
+          boat.timer = boat.timer - 1;
+          setBlocksUnloaded((blocks) => {
+            return blocks + 1;
+          });
+          if (boat.timer <= 0) {
+            boat.loaded = false;
+          }
+        } else {
+          boat.timer = boat.timer - 1;
+        }
+      });
+
+      let onWater = boats.filter((boat) => {
+        return ["river"].includes(boat.location);
+      }).length;
+
+      let finishedAtQuarry = boats.filter(
+        (boat) => boat.location === "quarry" && boat.loaded === true
+      );
+
+      while (onWater < 2) {
+        if (finishedAtQuarry.length) {
+          const boat = finishedAtQuarry.shift();
+          boat.location = "river";
+          boat.timer = timeOnWater;
+          onWater = onWater + 1;
+        } else {
+          const boat = boats.find(
+            (boat) => boat.location === "capitol" && boat.loaded === false
+          );
+          if (boat) {
+            boat.location = "river";
+            boat.timer = timeOnWater;
+            onWater = onWater + 1;
+          } else {
+            console.error(boats);
+            break;
+          }
+        }
+      }
+      return boats;
+    });
+  }, [timeRemaining, timeToUnload, timeToLoad, timeOnWater, setBoats]);
+
+  //I chose to use 'useRef' here rather than useState, because it is accessible globally and doesn't refresh on page re-render, meaning that it persists until the interval is done.
+  const simulationIntervalRef = useRef();
 
   const startSimulation = (time) => {
     setTimeRemaining(time);
     setSimulationStarted(true);
     setSimulationPaused(false);
 
-    intervalRef.current = setInterval((timeRemaining) => {
-      setTimeRemaining((timeRemaining) => {
-        const currentTimeRemaining = timeRemaining - 1;
+    simulationIntervalRef.current = setInterval(() => {
+      setTimeRemaining((_timeRemaining) => {
+        const currentTimeRemaining = _timeRemaining - 1;
 
         if (currentTimeRemaining <= 0) {
-          clearInterval(intervalRef.current);
-          intervalRef.current = undefined;
+          clearInterval(simulationIntervalRef.current);
+          simulationIntervalRef.current = undefined;
           setSimulationPaused(false);
           return currentTimeRemaining;
         } else return currentTimeRemaining;
@@ -38,8 +133,8 @@ function App() {
 
   const pauseOrResumeSimulation = () => {
     if (!simulationPaused) {
-      clearInterval(intervalRef.current);
-      intervalRef.current = undefined;
+      clearInterval(simulationIntervalRef.current);
+      simulationIntervalRef.current = undefined;
       setSimulationPaused(true);
     } else {
       startSimulation(timeRemaining);
@@ -47,23 +142,45 @@ function App() {
   };
 
   const restartSimulation = () => {
-    clearInterval(intervalRef.current);
-    intervalRef.current = undefined;
+    clearInterval(simulationIntervalRef.current);
+    simulationIntervalRef.current = undefined;
     setTimeRemaining(totalSimulationTime);
     setSimulationStarted(false);
     setSimulationPaused(false);
+    setBoats([]);
   };
   //----------------------------------------
   //--------------------
   //----------------------------------------
+  const boatsWaitingAtCapitol = useMemo(() => {
+    return boats.filter((boat) => {
+      return boat.location === "capitol" && boat.loaded === false;
+    });
+  }, [boats]);
 
-  const [numberOfBoats, setNumberOfBoats] = useState(20);
-  const [boatsAtCapitol, setBoatsAtCapitol] = useState(
-    Array.from(Array(numberOfBoats).keys())
-  );
-  const [boatsOnRiver, setBoatsOnRiver] = useState([]);
+  const boatsUnloadingAtCapitol = useMemo(() => {
+    return boats.filter((boat) => {
+      return boat.location === "capitol" && boat.loaded === true;
+    });
+  }, [boats]);
 
-  console.log(boatsAtCapitol);
+  const numberOfBoatsOnRiver = useMemo(() => {
+    return boats.filter((boat) => {
+      return ["river"].includes(boat.location);
+    });
+  }, [boats]);
+
+  const boatsWaitingAtQuarry = useMemo(() => {
+    return boats.filter((boat) => {
+      return boat.location === "quarry" && boat.loaded === true;
+    });
+  }, [boats]);
+
+  const boatsLoadingAtQuarry = useMemo(() => {
+    return boats.filter((boat) => {
+      return boat.location === "quarry" && boat.loaded === false;
+    });
+  }, [boats]);
 
   //----------------------------------------
   //--------------------
@@ -95,14 +212,24 @@ function App() {
           <button onClick={restartSimulation} disabled={!simulationStarted}>
             Restart Simulation
           </button>
-          <br></br>
+          <br />
           Time Remaining: {timeRemaining}
+          <br />
+          Blocks Unloaded: {blocksUnloaded}
         </p>
+        <hr />
+        Boats unloading at the Capitol: {boatsUnloadingAtCapitol.length}
+        <br />
+        Boats waiting at the Capitol: {boatsWaitingAtCapitol.length}
+        <br />
+        Boats on the Nile: {numberOfBoatsOnRiver.length}
+        <br />
+        Boats loading at the Quarry: {boatsLoadingAtQuarry.length}
+        <br />
+        Boats waiting at the Quarry: {boatsWaitingAtQuarry.length}
       </header>
     </div>
   );
 }
 
 export default App;
-
-// simulationPaused ? resumeSimulation :
